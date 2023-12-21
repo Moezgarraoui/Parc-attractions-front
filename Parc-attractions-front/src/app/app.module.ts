96ac4aa1ff9fd654702a363d03bf77b8aab89239
Moezgarraoui <garraouimouez@gmail.com>
2023-12-21 14:55:10 +0100

    component

--- Parc-attractions-front/src/app/app.module.ts
+++ Parc-attractions-front/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { BilleterieComponent } from './billeterie/billeterie.component';
+import { BilletterieComponent } from './billetterie/billetterie.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { ActualitesComponent } from './actualites/actualites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HotelComponent,
+    ReservationComponent,
+    RestaurantsComponent,
+    AccueilComponent,
+    BilleterieComponent,
+    BilletterieComponent,
+    ConnectionComponent,
+    ActualitesComponent
   ],
   imports: [
     BrowserModule,
