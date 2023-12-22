b17a8ce800b9329886627461c50e4b69689b30a8
MoezGarraoui <garraouimouez@gmail.com>
2023-12-22 11:48:25 +0100

    correction billetterie

--- Parc-attractions-front/src/app/app.module.ts
+++ Parc-attractions-front/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ActualitesComponent } from './actualites/actualites.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BilletterieComponent } from './billeterie/billetterie.component';
+import { BilletterieComponent } from './billetterie/billetterie.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { HotelComponent } from './hotel/hotel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PlanDuParcComponent } from './plan-du-parc/plan-du-parc.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
