73ac512ff2c78f74e1e074d498af235b6e8f18a4
cammiermon <camille.miermon.pro@gmail.com>
2024-01-04 11:47:12 +0100

    CSS sur la page hotel

--- Parc-attractions-front/src/app/hotel-page/hotel-page.component.ts
+++ Parc-attractions-front/src/app/hotel-page/hotel-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hotel-page',
@@ -10,5 +11,11 @@ export class HotelPageComponent implements OnInit{
   ngOnInit() {
     // Au moment de l'initialisation, défile vers le haut de la page
     window.scrollTo(0, 0);
+  }
+
+  constructor(private router: Router) {}
+
+  redirect(){
+    this.router.navigate(['/reservation_billets']);
   }
 }
