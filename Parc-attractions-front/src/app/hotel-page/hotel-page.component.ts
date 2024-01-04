a88bc34d6c86f18a55045cd93b748811ada8b60f
Ruxey <lorys.legohebel@gmail.com>
2024-01-04 16:11:07 +0100

    Barre recherche hotel + lien avec la BDD

--- Parc-attractions-front/src/app/hotel-page/hotel-page.component.ts
+++ Parc-attractions-front/src/app/hotel-page/hotel-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Hotel } from '../model';
+import { HotelService } from './hotel.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hotel-page',
@@ -7,15 +10,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./hotel-page.component.css']
 })
 export class HotelPageComponent implements OnInit{
+  hotelsArray:Hotel[]=[];
+  result: Hotel[] = [];
+  recherche: string = "";
 
+  constructor(private router: Router, private hotelService: HotelService) {
+  }
+  
   ngOnInit() {
     // Au moment de l'initialisation, défile vers le haut de la page
     window.scrollTo(0, 0);
+    this.loadHotel();
   }
 
-  constructor(private router: Router) {}
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
   redirect(){
     this.router.navigate(['/reservation_billets']);
+  }
+
+  rechercher(): Hotel[] {
+    return this.hotelsArray.filter((hotel) =>
+    (hotel.prixHotel !== undefined && hotel.prixHotel <= parseFloat(this.recherche)) ||
+    (hotel.nomHotel?.toLowerCase().includes(this.recherche.toLowerCase()))
+    );
+  }
+    
+  loadHotel() {
+    this.hotelService.findAll().subscribe(resp=>{this.hotelsArray=resp});
+    this.hotelService.findAll().subscribe(resp=>{this.result=resp});
   }
 }
